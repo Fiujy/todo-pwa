35687c129fcc741844ca8f83c2f7e8a7291595fc
Fiujy <momo.kerrouche31@gmail.com>
2024-11-29 15:28:45 +0100

    feat: ajout d'informations (text) et passage en fr

--- src/App.tsx
+++ src/App.tsx
@@ -2,49 +2,9 @@ import './App.css'
 import ToDoList from './components/ToDoList'
 
 function App() {
-
-  const requestNotificationPermission = async () => {
-    if ('Notification' in window) {
-      const permission = await Notification.requestPermission();
-      if (permission === 'granted') {
-        alert('Notifications activées !');
-      } else {
-        alert('Notifications refusées.');
-      }
-    } else {
-      alert('Les notifications ne sont pas supportées sur ce navigateur.');
-    }
-  };
-
-  const sendNotification = () => {
-    console.log("Send Notification");
-    if (Notification.permission === 'granted') {
-      console.log("Send Notification granted");
-      new Notification('Nouvelle tâche', {
-        body: 'Vous avez ajouté une nouvelle tâche à la liste ! 🎉',
-      });
-      console.log("Send Notification done");
-    } else {
-      alert('Les notifications ne sont pas activées ou ne sont pas encore demandées.');
-    }
-  };
-  
-
   return (
     <>
-      <div className="bg-slate-800 border">
-        <button
-          onClick={requestNotificationPermission}
-          className="bg-blue-500 text-white px-4 py-2 rounded mb-4"
-        >
-          Activer les Notifications
-        </button>
-        <button
-          onClick={sendNotification}
-          className="bg-green-500 text-white px-4 py-2 rounded"
-        >
-          Envoyer une Notification
-        </button>
+      <div className="bg-slate-800 border overflow-y-auto h-screen">
         <ToDoList />
       </div>
     </>
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -24,19 +24,27 @@ const ToDoList: React.FC = () => {
     };
 
     return (
-        <div className="w-full mx-auto p-4">
+        <div className="max-w-md mx-auto p-4">
             <h1 className="text-xl font-bold text-center mb-4">To-Do List</h1>
-            <div className="flex gap-2">
+            <div className='space-y-1 text-left'>
+                <p className='text-lg'>Les avantages de cette app : </p>
+                <p>1. Les données restent sauvegardées localement, après rechargement</p>
+                <p>2. Fonctionne aussi hors ligne</p>
+                <p>3. L'application est responsive (s'adapte à la taille d'écran)</p>
+                <p>4. Totalement autonome : fonctionne sans back-end</p>
+                <p>5. Peut-être ajoutée à votre écran d'accueil</p>
+            </div>
+            <div className="flex gap-2 pt-5">
                 <input type="text" value={input} onChange={(e) => setInput(e.target.value)} className="flex-1 border rounded p-2"
-                    placeholder="New task" />
-                <button onClick={addTask} className="bg-blue-500 text-white px-4 py-2 rounded">
-                    Add
+                    placeholder="Nouvelle tâche" />
+                <button onClick={addTask} className="bg-blue-700 text-white px-4 py-2 rounded border-none">
+                    Ajouter
                 </button>
             </div>
-            <ul className="mt-4 space-y-2">
+            <ul className="pt-5 space-y-2">
                 {tasks.map((task, index) => (
                     <li key={index} className="flex justify-between items-center bg-gray-700 p-2 rounded">{task}
-                        <button onClick={() => removeTask(index)} className="">Remove</button>
+                        <button onClick={() => removeTask(index)} className="border border-red-800">Supprimer</button>
                     </li>
                 ))}
             </ul>
